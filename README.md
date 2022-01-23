56dcf16c8f89d85bd1018f89c5a638e6aec075be
Tsegaye1a <75390075+Tsegaye1a@users.noreply.github.com>
2022-01-23 23:26:14 +0200

    best

---
+++ README.md
@@ -0,0 +1,48 @@
+
+Tefera Tsegaye Web Project
+
+The react page is well organized 
+
+-Used Django To implement all the required features.
+#### Implemented:
+    -Login/Logout
+    -Categories
+    -Payment
+    -Authentication
+    
+## How to Run
+    -
+    -pip install -r requirements.txt
+    -python manage.py makemigrations
+    -python manage.py migrate
+    -python manage.py runserver
+
+### Open Localhost on your browser
+
+
+
+#  Frontend
+Go the the right directory 
+
+cd frontend
+
+### `npm install`
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000]
+
+
+
+### `npm test`
+
+
+
+### `npm run build`
+
+
---
+++ api/products.py
@@ -0,0 +1,56 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Jacket ',
+        'image': '/images/airpods.jpg',
+        'description':
+        'Brand New Good Condition',
+        'brand': 'Peak Performance',
+        'category': 'Cloth',
+        'price': 20,
+        'countInStock': 2,
+        'rating': 5,
+        'numReviews': 2,
+    },
+    {
+        '_id': '2',
+        'name': 'Samsung Phone',
+        'image': '/images/airpods.jpg',
+        'description':
+        'Galaxy 7 New not used ',
+        'brand': 'Samsung',
+        'category': 'Electronics',
+        'price': 150,
+        'countInStock': 1,
+        'rating': 4.0,
+        'numReviews': 8,
+    },
+    {
+        '_id': '3',
+        'name': 'Smelly Matters',
+        'image': '/images/camera.jpg',
+        'description':
+        'Chaeap for survival',
+        'brand': 'Ikea',
+        'category': 'bed',
+        'price': 10,
+        'countInStock': 3,
+        'rating': 3,
+        'numReviews': 2,
+    },
+    {
+        '_id': '4',
+        'name': 'Sony Playstation 4 n',
+        'image': '/images/playstation.jpg',
+        'description':
+        'The ultimate home entertainment ',
+        'brand': 'Sony',
+        'category': 'Electronics',
+        'price': 240,
+        'countInStock': 6,
+        'rating': 5,
+        'numReviews': 1,
+    },
+    
+       
+]
